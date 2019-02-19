38236a00432dac8cd4865e40604c57a38f851617
Grant <grant.a.ballmer@gmail.com>
2019-02-19 01:37:27 +0000

    changed back view of services' cards

--- src/components/services/Card.js
+++ src/components/services/Card.js
@@ -4,8 +4,8 @@ import { Link } from "gatsby";
 
 const Card = ({ frontImage, tinyImage, icon, service, management, check }) => {
 
-  const front = <img src={frontImage} alt="" style={{ display: 'none' }} />;
-  console.log(front.complete);
+  // const front = <img src={frontImage} alt="" style={{ display: 'none' }} />;
+  // console.log(front.complete);
 
   return (
     <Link to={`/services/photography`} className={`card card__${service}`}>
@@ -21,45 +21,7 @@ const Card = ({ frontImage, tinyImage, icon, service, management, check }) => {
       </div>
       
       <div className={`card__side card__side--back backImage__${service}`}>
-        {!management 
-          ? <button to={`/services/${service}`} className="btn btn-gallery">View Gallery</button>
-          :
-          <React.Fragment>
-            <div className="overlay-background-image management-background"/>
-            <div className="management-info">
-            
-              <div className="management-info__item">
-              
-                <div className="management-info__item--top">
-                  <img src={check} alt="check" />
-                  <h3>Management</h3>
-                </div>
-  
-                <p>-Email for quote</p>
-              </div>
-              
-              <div className="management-info__item">
-                <div className="management-info__item--top">
-                  <img src={check} alt="check" />
-                  <h3>Booking</h3>
-                </div>
-  
-                <p>-Email for quote</p>
-              </div>
-              
-              <div className="management-info__item">
-                <div className="management-info__item--top">
-                  <img src={check} alt="check" />
-                  <h3>Tour Management</h3>
-                </div>
-  
-                <p>-$65-$150 a day</p>
-              </div>
-              
-            </div>
-          </React.Fragment>
-          
-        }  
+        <button to={`/services/${service}`} className="btn btn-gallery">View Gallery</button>
       </div>
       
     </Link>
@@ -67,3 +29,44 @@ const Card = ({ frontImage, tinyImage, icon, service, management, check }) => {
 };
 
 export default Card;
+
+
+// {!management 
+//           ? <button to={`/services/${service}`} className="btn btn-gallery">View Gallery</button>
+//           :
+//           <React.Fragment>
+//             <div className="overlay-background-image management-background"/>
+//             <div className="management-info">
+
+//               <div className="management-info__item">
+
+//                 <div className="management-info__item--top">
+//                   <img src={check} alt="check" />
+//                   <h3>Management</h3>
+//                 </div>
+
+//                 <p>-Email for quote</p>
+//               </div>
+
+//               <div className="management-info__item">
+//                 <div className="management-info__item--top">
+//                   <img src={check} alt="check" />
+//                   <h3>Booking</h3>
+//                 </div>
+
+//                 <p>-Email for quote</p>
+//               </div>
+
+//               <div className="management-info__item">
+//                 <div className="management-info__item--top">
+//                   <img src={check} alt="check" />
+//                   <h3>Tour Management</h3>
+//                 </div>
+
+//                 <p>-$65-$150 a day</p>
+//               </div>
+
+//             </div>
+//           </React.Fragment>
+
+//         }
