89d00fe42b2404fd1a1035a7eb6e2abee2ac0bcb
Grant <grant.a.ballmer@gmail.com>
2019-02-20 00:01:21 +0000

    remove unused previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,15 +5,15 @@ import CMS from 'netlify-cms';
 // CMS.registerPreviewStyle(styles);
 
 
-import AboutPagePreview from './preview-templates/AboutPagePreview';
-import BlogPostPreview from './preview-templates/BlogPostPreview';
-import ProductPagePreview from './preview-templates/ProductPagePreview';
+// import AboutPagePreview from './preview-templates/AboutPagePreview';
+// import BlogPostPreview from './preview-templates/BlogPostPreview';
+// import ProductPagePreview from './preview-templates/ProductPagePreview';
 
 import GalleryPagePreview from "./preview-templates/GalleryPreview";
 
-CMS.registerPreviewTemplate('about', AboutPagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
-CMS.registerPreviewTemplate('blog', BlogPostPreview);
+// CMS.registerPreviewTemplate('about', AboutPagePreview);
+// CMS.registerPreviewTemplate('products', ProductPagePreview);
+// CMS.registerPreviewTemplate('blog', BlogPostPreview);
 CMS.registerPreviewTemplate('gallery', GalleryPagePreview);
 // CMS.registerPreviewTemplate('gallery', ({ entry }) => (
 //   <GalleryPageTemplate {...entry.toJS().data} />
