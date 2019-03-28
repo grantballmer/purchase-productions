1f30cadd88ec22f90c479b994523f0774680b930
Grant Ballmer <grant.a.ballmer@gmail.com>
2019-03-28 09:29:02 -0400

    add logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import './scss/navbar.scss';
+import React from "react";
+import { Link } from "gatsby";
+import Logo from "./images/logo.jpg";
+import "./scss/navbar.scss";
 
-import { FaFacebookSquare, FaInstagram, FaSoundcloud } from 'react-icons/fa';
+import { FaFacebookSquare, FaInstagram, FaSoundcloud } from "react-icons/fa";
 // import github from '../img/github-icon.svg'
 // import logo from '../img/logo.svg'
 
@@ -18,67 +19,96 @@ class Navbar extends React.Component {
   handleClick = e => {
     const { openMenu } = this.state;
     this.setState({ openMenu: !openMenu });
-  }
+  };
 
   removeSideNav = () => {
     const { openMenu } = this.state;
 
     if (!openMenu) return;
     this.setState({ openMenu: !openMenu });
-
-  }
+  };
 
   render() {
     const { openMenu } = this.state;
-    const slideClass = openMenu ? 'slideIn' : '';
-    const menuClass = openMenu ? 'open' : '';
+    const slideClass = openMenu ? "slideIn" : "";
+    const menuClass = openMenu ? "open" : "";
 
     return (
       <nav>
-      
         <Link to="/" className="logo" onClick={this.removeSideNav}>
-            Purchase Productions, LLC
+          <img src={Logo} alt="logo" />
         </Link>
-                
+
         <div className={`nav-menu ${menuClass}`} onClick={this.handleClick}>
-          <span></span>
-          <span></span>
-          <span></span>
+          <span />
+          <span />
+          <span />
         </div>
-        
+
         <div className={`sideNav ${slideClass}`}>
-          <Link to="/" onClick={this.handleClick}>Home</Link>
-          <Link to="/about" onClick={this.handleClick}>About</Link>
-          <Link to="/roster" onClick={this.handleClick}>Roster</Link>
-          <Link to="/services" onClick={this.handleClick}>Services</Link>
-          <Link to="/podcast" onClick={this.handleClick}>Podcast</Link>
-          <Link to="/contact" onClick={this.handleClick}>Contact</Link>
+          <Link to="/" onClick={this.handleClick}>
+            Home
+          </Link>
+          <Link to="/about" onClick={this.handleClick}>
+            About
+          </Link>
+          <Link to="/roster" onClick={this.handleClick}>
+            Roster
+          </Link>
+          <Link to="/services" onClick={this.handleClick}>
+            Services
+          </Link>
+          <Link to="/podcast" onClick={this.handleClick}>
+            Podcast
+          </Link>
+          <Link to="/contact" onClick={this.handleClick}>
+            Contact
+          </Link>
         </div>
-        
+
         <div className="nav-container">
-        
           <div className="nav-middle">
-            <Link to="/about" className="nav-middle__links">About</Link>
-            <Link to="/roster"className="nav-middle__links">Roster</Link>
-            <Link to="/services" className="nav-middle__links">Services</Link>
-            <Link to="/podcast" className="nav-middle__links">Podcast</Link>
-            <Link to="/contact"className="nav-middle__links">Contact</Link>
+            <Link to="/about" className="nav-middle__links">
+              About
+            </Link>
+            <Link to="/roster" className="nav-middle__links">
+              Roster
+            </Link>
+            <Link to="/services" className="nav-middle__links">
+              Services
+            </Link>
+            <Link to="/podcast" className="nav-middle__links">
+              Podcast
+            </Link>
+            <Link to="/contact" className="nav-middle__links">
+              Contact
+            </Link>
           </div>
-          
+
           <div className="nav-social-media">
-            <a href="https://www.facebook.com/pg/purchaseproductionsllc/about/?ref=page_internal" target="_blank" rel="noopener noreferrer" >
+            <a
+              href="https://www.facebook.com/pg/purchaseproductionsllc/about/?ref=page_internal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaFacebookSquare />
             </a>
-            <a href="https://www.instagram.com/purchaseproductions/" target="_blank" rel="noopener noreferrer" >
+            <a
+              href="https://www.instagram.com/purchaseproductions/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaInstagram />
             </a>
-            <a href="https://soundcloud.com/purchaseproductions?" target="_blank" rel="noopener noreferrer" >
+            <a
+              href="https://soundcloud.com/purchaseproductions?"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaSoundcloud />
             </a>
           </div>
-          
         </div>
-        
       </nav>
     );
   }
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import Img from 'gatsby-image';
-import { FaSpotify, FaYoutube, FaItunesNote, FaFacebookSquare, FaInstagram } from 'react-icons/fa';
+import React from "react";
+import Img from "gatsby-image";
+// import BackgroundImage from "gatsby-background-image";
+import {
+  FaSpotify,
+  FaYoutube,
+  FaItunesNote,
+  FaFacebookSquare,
+  FaInstagram
+} from "react-icons/fa";
 
 import "./profile.scss";
 import { details } from "./profileDetails";
@@ -9,20 +16,34 @@ import { details } from "./profileDetails";
 const Profile = ({ band, image }) => {
   const profileDetails = details[band];
 
-  const { name, headers, musicType, info, socialMedia, management } = profileDetails;
+  const {
+    name,
+    headers,
+    musicType,
+    info,
+    socialMedia,
+    management
+  } = profileDetails;
   const { spotify, youtube, itunes, facebook, instagram } = socialMedia;
 
-  const headings = headers.map(header => (<h2 key={header}>{header}</h2>));
+  const headings = headers.map(header => <h2 key={header}>{header}</h2>);
 
-  const infoParagraphs = info.map((text, index) => (<p key={index}>{text}</p>));
+  const infoParagraphs = info.map((text, index) => <p key={index}>{text}</p>);
 
   const managementDivs = management.map(person => {
     const { title, name, email } = person;
     return (
       <div key={`${name + title}`} className="contact-information">
-        <h5>{title}: <span>{name}</span></h5>
-        <a className="contact-information__email" href={`mailto:${email}`} target="_blank" rel="noopener noreferrer">
-          <i className="fas fa-envelope"></i>
+        <h5>
+          {title}: <span>{name}</span>
+        </h5>
+        <a
+          className="contact-information__email"
+          href={`mailto:${email}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fas fa-envelope" />
           <p>{email}</p>
         </a>
       </div>
@@ -32,84 +53,105 @@ const Profile = ({ band, image }) => {
   return (
     <section className="profile-container">
       <div className="profile-hero-wrapper">
-      
         <div className="svg-container svg-container__profile">
-          <svg width='100%' height='100%' viewBox="0 0 100 100" preserveAspectRatio="none">
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+          >
             <polygon points="0,94 100,98 100,100 0,100" />
           </svg>
         </div>
-        
+
         <div className={`profile-hero`}>
-          <Img fluid={image} style={{ position: 'static' }} />
-          {headings} 
+          <Img fluid={image} style={{ position: "static" }} />
+          {/* <BackgroundImage className="gatsby-background" fluid={image} /> */}
+          {headings}
         </div>
-        
       </div>
-      
+
       <div className="profile-about">
-        
         <div className="profile-about__info">
-        
-          <h3>{name}</h3> 
+          <h3>{name}</h3>
           <h4>{musicType}</h4>
-          
+
           {infoParagraphs}
-          
         </div>
-        
+
         <div className="profile-about__music">
-          
-          
-          <a className="profile-about__music--platform" href={spotify} target="_blank" rel="noopener noreferrer">
+          <a
+            className="profile-about__music--platform"
+            href={spotify}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaSpotify className="profile-icon" />
             <h4>Spotify</h4>
           </a>
-          
+
           {/* FaYoutube, FaItunesNote, FaFacebookSquare, FaInstagram */}
 
-          <a className="profile-about__music--platform" href={youtube} target="_blank" rel="noopener noreferrer">
+          <a
+            className="profile-about__music--platform"
+            href={youtube}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaYoutube className="profile-icon" />
             <h4>Youtube</h4>
           </a>
-          
-          <a className="profile-about__music--platform" href={itunes} target="_blank" rel="noopener noreferrer">
+
+          <a
+            className="profile-about__music--platform"
+            href={itunes}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaItunesNote className="profile-icon" />
             <h4>iTunes</h4>
           </a>
-          
-          <a className="profile-about__music--platform" href={facebook} target="_blank" rel="noopener noreferrer">
+
+          <a
+            className="profile-about__music--platform"
+            href={facebook}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaFacebookSquare className="profile-icon" />
             <h4>Facebook</h4>
           </a>
-          
-          <a className="profile-about__music--platform" href={instagram} target="_blank" rel="noopener noreferrer">
+
+          <a
+            className="profile-about__music--platform"
+            href={instagram}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaInstagram className="profile-icon" />
             <h4>Instagram</h4>
           </a>
-
         </div>
-        
       </div>
-      
+
       <div className="profile-contact">
-        <div className="svg-container svg-container__profile svg-container__contact" >
-          <svg width='100%' height='100%' viewBox="0 0 100 100" preserveAspectRatio="none">
+        <div className="svg-container svg-container__profile svg-container__contact">
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+          >
             <polygon points="0,0 100,0 100,12 0,4" />
           </svg>
         </div>
         <h4>Contact Info</h4>
-        
-        <div className="profile-contact__details">
-        
-          {managementDivs}
-          
-        </div>
-        
-        {/* empty div to allow space-between to center contact info */} 
-        <div></div>
-        
+
+        <div className="profile-contact__details">{managementDivs}</div>
+
+        {/* empty div to allow space-between to center contact info */}
+        <div />
       </div>
-    
     </section>
   );
 };
