1da6f4e0390130f795b64e4836af0f35dd73c4a0
Grant Ballmer <grant.a.ballmer@gmail.com>
2019-04-04 11:21:46 -0400

    add 404 route

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import "./scss/404.scss";
+import Layout from "../components/Layout";
 
-const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
-)
+class NotFoundPage extends React.Component {
+  handleClick = () => {
+    window.history.back();
+  };
 
-export default NotFoundPage
+  render() {
+    return (
+      <Layout>
+        <div className="page-not-found">
+          <div className="page-not-found__hero" />
+          <div className="page-not-found__text">
+            <h1>404</h1>
+            <h2>Page not found...</h2>
+            {/* <p>You just hit a route that doesn&#39;t exist...</p> */}
+            <button className="btn btn-link" onClick={this.handleClick}>
+              Previous Page
+            </button>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default NotFoundPage;
