511a7baf3825f7628676a362a0415579e05fb2fe
Grant Ballmer <grant.a.ballmer@gmail.com>
2019-04-16 14:22:54 -0400

    remove podcast video

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -7,14 +7,7 @@ const Podcast = () => {
     <Layout>
       <section className="podcast">
         <div className="podcast__hero" />
-        <iframe
-          width="100%"
-          height="166"
-          scrolling="no"
-          frameborder="no"
-          title="soundcloud"
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/34019569&amp;color=0066cc"
-        />
+
         <h2 className="podcast__text">Coming Soon</h2>
       </section>
     </Layout>
