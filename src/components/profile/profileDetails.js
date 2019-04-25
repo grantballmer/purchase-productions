a9c587b02063768f9cfa0e8aa6ffdc799414fcd2
Grant Ballmer <grant.a.ballmer@gmail.com>
2019-04-25 19:37:31 -0400

    update chirp page

--- src/components/profile/profileDetails.js
+++ src/components/profile/profileDetails.js
@@ -89,22 +89,21 @@ export const details = {
   chirp: {
     name: "Chirp",
     headers: ["Chirp"],
-    musicType: "Funk/Jazz/Fusion",
+    musicType: "Progressive Rock / Funk / Fusion",
     heroClass: "profile-hero__chirp",
     info: [
       `Based in Ann Arbor, MI, Chirp is a four-piece band that does their fair share of genre-hopping. 
-      Often playing in the styles of progressive rock/funk and jazz-fusion, their sound maintains a melodic center.
-      The group features a wide array of originals and covers in their live sets and puts together dynamic, 
-      high-energy shows. Though their music ties in many technical elements, an equal emphasis is put on 
-      grooving and keeping people moving. Chirp takes influence from Snarky Puppy, Steely Dan, Led Zeppelin, James Brown, 
-      Yes, Daft Punk, Vulfpeck, and The Beatles to name a few.`,
+      Often playing in the styles of progressive rock/funk and jazz-fusion, their sound maintains a melodic center. 
+      The group features a wide array of originals and covers in their live sets and puts together dynamic, high-energy shows. 
+      Though their music ties in many technical elements, an equal emphasis is put on grooving and keeping people moving. 
+      Chirp takes influence from Snarky Puppy, Steely Dan, Led Zeppelin, James Brown, Yes, Daft Punk, Vulfpeck, and The Beatles to name a few. `,
 
       `Chirp has been hard at work since their formation in 2015, playing over 200 shows in 3 years. 
-      Their fresh sound has taken them touring across the Midwest to play a few large variety of venues 
-      and festivals. In addition to several lives sets from shows, Chirp has released three live-recorded EPs: "Live from Brownstown" (in-studio, 2016), 
-      "Live at Willis Sound" (in-studio, 2017), and "Live at Ann Arbor Summer Festival" (2018). 
-      All of which have been met with great reception and have helped along with live performances to 
-      build an extremely enthusiastic fan base. Chirp is Jay Frydenlund, Brian Long, Ken Ball, and John Gorine.`
+      Their fresh sound has taken them touring across the Midwest to play a large variety of venues and festivals. 
+      Chirp released their self-titled debut studio album in March 2019 as well as three live-recorded EPs: 
+      "Live from Brownstown" (in-studio, 2016), "Live at Willis Sound" (in-studio, 2017), and “Live at Ann Arbor Summer Festival” (2018). 
+      All of which have been met with great reception and have helped along with live performances to build an extremely enthusiastic fan base. 
+      Chirp is Jay Frydenlund, Brian Long, Ken Ball, and John Gorine.`
     ],
     socialMedia: {
       spotify: "https://open.spotify.com/artist/2Lljzm1nd6U9hGbUGouL0u",
