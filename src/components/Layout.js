e59b8ce787381be8259431691944d6b307d6f5af
Grant Ballmer <grant.a.ballmer@gmail.com>
2019-05-09 13:43:51 -0400

    add patreon link to podcast page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,38 +1,62 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import Helmet from "react-helmet";
+import { StaticQuery, graphql } from "gatsby";
 
-import Navbar from '../components/Navbar';
-import './all.scss';
+import Navbar from "../components/Navbar";
+import "./all.scss";
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
     query={graphql`
       query HeadingQuery {
-          site {
-            siteMetadata {
-              title,
-              description,
-            }
+        site {
+          siteMetadata {
+            title
+            description
           }
         }
+      }
     `}
     render={data => (
       <div>
         <Helmet>
           <html lang="en" />
           <title>Purchase Productions</title>
-          <meta name="description" content={data.site.siteMetadata.description} />
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,700" rel="stylesheet" />
-          
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
-	        <meta name="theme-color" content="#fff" />
+          <meta
+            name="description"
+            content="Purchase Productions LLC. Michigan based agency for band management, tour management, design, and music/band/concert photography"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,700"
+            rel="stylesheet"
+          />
 
-	        <meta property="og:type" content="business.business" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/img/favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/img/favicon-16x16.png"
+            sizes="16x16"
+          />
+
+          <link
+            rel="mask-icon"
+            href="/img/safari-pinned-tab.svg"
+            color="#ff4400"
+          />
+          <meta name="theme-color" content="#fff" />
+
+          <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-image.jpg" />
@@ -42,6 +66,6 @@ const TemplateWrapper = ({ children }) => (
       </div>
     )}
   />
-)
+);
 
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -9,6 +9,16 @@ const Podcast = () => {
         <div className="podcast__hero" />
 
         <h2 className="podcast__text">Coming Soon</h2>
+        <h3>
+          Become a patron today:{" "}
+          <a
+            href="https://www.patreon.com/LifeThroughMusic?fbclid=IwAR0DDKNeJlPKKeUJBEbRw7nXAuiAOad-lzawY-C0F-OKen1fIbPhedpuulc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Life Through Music
+          </a>
+        </h3>
       </section>
     </Layout>
   );
