8a95437967dbf771757929df8cf4f9081bae2e20
Grant Ballmer <grant.a.ballmer@gmail.com>
2019-05-24 10:52:23 -0400

    delete commented out code

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -60,35 +60,7 @@ const Podcast = ({ data }) => {
 
         <h1>Episodes</h1>
 
-        <div className="episodes">
-          {episodes}
-          {/* <Link
-            to="/podcast/special-music-episode"
-            className="episode-container"
-          >
-            <div className="episode-counter">02</div>
-            <h2>Special Music Episode</h2>
-            <p>May 22nd, 2019</p>
-            <p>
-              Special music episode featuring the songs of bands from Michigan,
-              Ohio, and Colorado.
-            </p>
-          </Link>
-
-          <Link
-            to="/podcast/chris-bota-and-john-nowak"
-            className="episode-container"
-          >
-            <div className="episode-counter">01</div>
-            <h2>Chris Bota & John Nowak of Desmond Jones</h2>
-            <p>May 20th, 2019</p>
-            <p>
-              On episode 001, featuring Chris Bota and John Nowak of Desmond
-              Jones, we chat about the early years and members and tour life.
-              Listen now!
-            </p>
-          </Link> */}
-        </div>
+        <div className="episodes">{episodes}</div>
       </section>
     </Layout>
   );
