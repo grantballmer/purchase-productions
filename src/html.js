d27453bfdadc1d38277a2feb7d86d322a9503b52
Grant Ballmer <grant.a.ballmer@gmail.com>
2019-08-18 11:23:43 -0400

    remove meta title from html.js

--- src/html.js
+++ src/html.js
@@ -17,7 +17,6 @@ export default function HTML(props) {
           media="screen"
         />
         {props.headComponents}
-        <meta name="title" content="Purchase Productions LLC." />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
